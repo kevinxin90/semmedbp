0733086eb3b4cdbdd891cab3543eeb58e1fb36de
jiwen xin <kevinxin@scripps.edu>
2019-11-15 10:59:35 -0800

    add name field, remove duplicate, enable reverse

--- parser.py
+++ parser.py
@@ -26,11 +26,9 @@ def load_data(data_folder):
         next(csv_reader)
         for _item in csv_reader:
             group_by_semmantic_dict[_item[-2]].append(_item[-1])
-            id_type_mapping[_item[-1]] = _item[-2]
+            id_type_mapping[_item[-1]] = {'type': _item[-2], 'name': _item[1]}
     bp_related = {}
-    parsed_type_list = ['disease_or_phenotypic_feature', 'gene',
-                        'protein', 'chemical_substance', 'phenotypic_feature',
-                        'anatomical_entity']
+    unique_assocs = set()
     with open(edges_path) as f:
         csv_reader = csv.reader(f, delimiter=',')
         next(csv_reader)
@@ -38,26 +36,32 @@ def load_data(data_folder):
             if _item[4] in group_by_semmantic_dict['biological_process_or_activity']:
                 if _item[4] not in bp_related:
                     bp_related[_item[4]] = {'_id': _item[4][5:],
-                                                  'umls': _item[4][5:]}
+                                            'umls': _item[4][5:],
+                                            'name': id_type_mapping[_item[4]]['name']}
                 pred = _item[0].lower()
                 semantic_type = id_type_mapping[_item[5]]
-                if semantic_type not in parsed_type_list:
-                    if pred not in bp_related[_item[4]]:
-                        bp_related[_item[4]][pred] = {}
-                    if semantic_type not in bp_related[_item[4]][pred]:
-                        bp_related[_item[4]][pred][semantic_type] = []
+                if pred not in bp_related[_item[4]]:
+                    bp_related[_item[4]][pred] = {}
+                if semantic_type not in bp_related[_item[4]][pred]:
+                    bp_related[_item[4]][pred][semantic_type] = []
+                assoc = _item[4] + pred + str(_item[1]) + _item[5]
+                if assoc not in unique_assocs:
+                    unique_assocs.add(assoc)
                     bp_related[_item[4]][pred][semantic_type].append({'pmid': _item[1].split(';'), 'umls': _item[5][5:]})
             elif _item[5] in group_by_semmantic_dict['biological_process_or_activity']:
                 if _item[5] not in bp_related:
                     bp_related[_item[5]] = {'_id': _item[5][5:],
-                                                  'umls': _item[5][5:]}
+                                            'umls': _item[5][5:],
+                                            'name': id_type_mapping[_item[5]]['name']}
                 pred = _item[0].lower() + '_reverse'
                 semantic_type = id_type_mapping[_item[4]]
-                if semantic_type not in parsed_type_list:
-                    if pred not in bp_related[_item[5]]:
-                        bp_related[_item[5]][pred] = {}
-                    if semantic_type not in bp_related[_item[5]][pred]:
-                        bp_related[_item[5]][pred][semantic_type] = []
+                if pred not in bp_related[_item[5]]:
+                    bp_related[_item[5]][pred] = {}
+                if semantic_type not in bp_related[_item[5]][pred]:
+                    bp_related[_item[5]][pred][semantic_type] = []
+                assoc = _item[5] + pred + str(_item[1]) + _item[4]
+                if assoc not in unique_assocs:
+                    unique_assocs.add(assoc)
                     bp_related[_item[5]][pred][semantic_type].append({'pmid': _item[1].split(';'), 'umls': _item[4][5:]})
     for v in bp_related.values():
         yield v
